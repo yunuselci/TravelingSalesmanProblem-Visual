433b59173dfd38ea55734884a08d18c225bd5962
yunuselci <mryunuselci@gmail.com>
2021-06-12 22:03:12 +0300

    WIP1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,20 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     final static File tspFile = new File("C:\\Users\\YUNUS\\IdeaProjects\\Cmp\\src\\com\\company\\ca4663.tsp");
-    static List<Points> pointsList = new ArrayList<>();
+    static List<Points> nearestNeighborPointList = new ArrayList<>();
     static List<Points> originalPointsList = new ArrayList<>();
-    static List<Integer> rota = new ArrayList<>();
-    static int mesafe = 0;
+    static List<Integer> routeForNearestNeighbor = new ArrayList<>();
+    static List<Integer> list1 = new ArrayList<>();
+    static HashMap<Integer, Double> hashMap = new HashMap<>();
+
+    static int totalLenght = 0;
     static int cityNumber = 0;
     static Points MaxPoint = new Points(0, 0, 0);
 
@@ -25,7 +28,7 @@ public class Main {
                 int x = Integer.parseInt(String.valueOf(Math.round(Float.parseFloat(details[1]))));
                 int y = Integer.parseInt(String.valueOf(Math.round(Float.parseFloat(details[2]))));
                 Points points = new Points(cityNumber, x, y);
-                pointsList.add(points);
+                nearestNeighborPointList.add(points);
                 originalPointsList.add(points);
             }
         } catch (Exception e) {
@@ -33,39 +36,128 @@ public class Main {
         }
     }
 
-    static void nearestNeighbor() {
-        double distance;
-        double nearest = Integer.MAX_VALUE;
-
-
-        rota.add(1700);
-        for (int j = 0; j < pointsList.size() - 1; j++) {
-            double x1 = pointsList.get((rota.get(j)) - 1).x;
-            double y1 = pointsList.get((rota.get(j)) - 1).y;
-            pointsList.remove((rota.get(j)) - 1);
-            pointsList.add((rota.get(j) - 1), MaxPoint);
-            for (int i = 0; i < pointsList.size(); i++) {
-                double x2 = pointsList.get(i).x;
-                double y2 = pointsList.get(i).y;
-                distance = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
-                if (distance < nearest) {
-                    nearest = distance;
-                    cityNumber = pointsList.get(i).cityNumber;
-
+    static void divideAndConq() {
+        for (int k = 1; k < 3; k++) {
+            double onetoall = 0;
+            double x1, x2, y1, y2;
+            double distance;
+            hashMap.clear();
+            for (int j = 0; j < originalPointsList.size(); j++) {
+                if (originalPointsList.get(j).x != 0) {
+                    x1 = originalPointsList.get(j).x;
+                    y1 = originalPointsList.get(j).y;
+                    for (int i = 0; i < originalPointsList.size(); i++) {
+                        if (originalPointsList.get(i).x != 0) {
+                            x2 = originalPointsList.get(i).x;
+                            y2 = originalPointsList.get(i).y;
+                            distance = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+                            onetoall = onetoall + distance;
+                        }
+                    }
+                    hashMap.put(j, (onetoall / originalPointsList.size()));
+                    onetoall = 0;
                 }
             }
-            mesafe += nearest;
-            rota.add(cityNumber);
-            nearest = Integer.MAX_VALUE;
-        }
 
+            Map<Integer, Double> hm1 = sortByValue(hashMap);
+
+            int tempCounter = 0;
+            for (Map.Entry<Integer, Double> en : hm1.entrySet()) {//4662
+                tempCounter++;
+                if (tempCounter <= (originalPointsList.size() / 2*k)) {
+                    list1.add((en.getKey() + 1));
+                    int temp = en.getKey();
+                    originalPointsList.remove(temp);
+                    originalPointsList.add(en.getKey(), MaxPoint);
+                }
+//            System.out.println("Bekayin Sayisi:" + tempCounter +"  Key = " + (en.getKey() + 1) +
+//                    ", Value = " + en.getValue());
+
+            }
+            hm1.clear();
+            list1.add(0);
+
+
+        }
 
     }
 
+
+    //Nearest Neighbor Algorithm
+    static void calculateRouteBasedOnNearPoints(List<Points> pList, List<Integer> route, int startingPoint) {
+        double distance;
+        double nearest = Integer.MAX_VALUE;
+        totalLenght = 0;
+        cityNumber = 0;
+        route.add(startingPoint);
+        for (int j = 0; j < pList.size() - 1; j++) {
+            double x1 = pList.get((route.get(j)) - 1).x;
+            double y1 = pList.get((route.get(j)) - 1).y;
+            pList.remove((route.get(j)) - 1);
+            pList.add((route.get(j) - 1), MaxPoint);
+            for (Points points : pList) {
+                double x2 = points.x;
+                double y2 = points.y;
+                distance = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+                if (distance < nearest) {
+                    nearest = distance;
+                    cityNumber = points.cityNumber;
+
+                }
+            }
+            totalLenght += nearest;
+            route.add(cityNumber);
+            nearest = Integer.MAX_VALUE;
+        }
+    }
+
+
+    public static HashMap<Integer, Double> sortByValue(HashMap<Integer, Double> hm) {
+        // Create a list from elements of HashMap
+        List<Map.Entry<Integer, Double>> list =
+                new LinkedList<>(hm.entrySet());
+        // Sort the list
+        list.sort(Map.Entry.comparingByValue());
+        // put data from sorted list to hashmap
+        HashMap<Integer, Double> temp = new LinkedHashMap<>();
+        for (Map.Entry<Integer, Double> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+
+//    public static Set<Integer> findDuplicates(List<Integer> listContainingDuplicates)
+//    {
+//        final Set<Integer> setToReturn = new HashSet<>();
+//        final Set<Integer> set1 = new HashSet<>();
+//
+//        for (Integer yourInt : listContainingDuplicates)
+//        {
+//            if (!set1.add(yourInt))
+//            {
+//                setToReturn.add(yourInt);
+//            }
+//        }
+//        return setToReturn;
+//    }
     public static void main(String[] args) {
         fileRead();
-        nearestNeighbor();
 
+        divideAndConq();
+
+
+
+
+// print the sorted hashmap
+//        for (Map.Entry<Integer, Double> en : hm1.entrySet()) {
+//            System.out.println("Key = " + en.getKey() +
+//                    ", Value = " + en.getValue());
+//        }
+
+//        hashMap.forEach((key, value) -> System.out.println(key + " " + value));
+//        calculateRouteBasedOnNearPoints(nearestNeighborPointList, routeForNearestNeighbor, 1700);
+//
+//
         JFrame jFrame = new JFrame("Screen");
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Screen screen = new Screen();
@@ -74,10 +166,11 @@ public class Main {
         jFrame.setLocationRelativeTo(null);
         jFrame.setSize(1920, 1080);
         jFrame.setVisible(true);
-        System.out.println("Total Length: " + mesafe);
-        for (Integer integer : rota) {
-            System.out.print(integer + "->");
-        }
+
+//        System.out.println("Total Length: " + totalLenght);
+//        for (Integer integer : routeForNearestNeighbor) {
+//            System.out.print(integer + "->");
+//        }
 
     }
 }
--- src/com/company/Screen.java
+++ src/com/company/Screen.java
@@ -15,27 +15,46 @@ public class Screen extends JPanel {
 
         g.setColor(Color.RED);
 
+        for (int i = 0; i < Main.list1.size(); i++) {
 
+            if (Main.list1.get(i) == 0) {
 
-        for (int i = 0; i < Main.originalPointsList.size(); i++) {
+                g.setColor(Color.BLUE);
 
-            g.fillOval(Main.originalPointsList.get(i).y /100,(Main.originalPointsList.get(i).x / 100), 5, 5);
+            } else {
+
+                g.fillOval(Main.nearestNeighborPointList.get((Main.list1.get(i)) - 1).y / 100, (Main.nearestNeighborPointList.get((Main.list1.get(i)) - 1).x / 100), 5, 5);
+
+//                g.drawString(String.valueOf(Main.nearestNeighborPointList.get((Main.list1.get(i)) - 1).cityNumber),
+//                        (Main.nearestNeighborPointList.get((Main.list1.get(i)) - 1).y / 100),
+//                        Main.nearestNeighborPointList.get((Main.list1.get(i)) - 1).x / 100);
+//
+
+            }
 
         }
 
 
-        g.setColor(Color.BLACK);
+//        g.setColor(Color.BLUE);
+//        for (int i = 0; i < Main.originalPointsList.size(); i++) {
+//
+//            g.fillOval(Main.originalPointsList.get(i).y /100,(Main.originalPointsList.get(i).x / 100), 5, 5);
+//
+//        }
 
 
-        for (int j = 0; j < Main.rota.size()-2; j++) {
-            g.drawLine((Main.originalPointsList.get(Main.rota.get(j)-1).y )/100,
-                    ((Main.originalPointsList.get(Main.rota.get(j)-1).x )/100),
-                    (Main.originalPointsList.get(Main.rota.get(j+1)-1).y ) /100,
-                   ((Main.originalPointsList.get(Main.rota.get(j+1)-1).x )/100));
-        }
+//        g.setColor(Color.BLACK);
+
+
+//        for (int j = 0; j < Main.routeForNearestNeighbor.size()-2; j++) {
+//            g.drawLine((Main.originalPointsList.get(Main.routeForNearestNeighbor.get(j)-1).y )/100,
+//                    ((Main.originalPointsList.get(Main.routeForNearestNeighbor.get(j)-1).x )/100),
+//                    (Main.originalPointsList.get(Main.routeForNearestNeighbor.get(j+1)-1).y ) /100,
+//                   ((Main.originalPointsList.get(Main.routeForNearestNeighbor.get(j+1)-1).x )/100));
+//        }
         g.scale(-1.0, -1.0);
         g.translate(-1920, -1080);
-        g.drawString("Total Length: "+ Main.mesafe, 1000,100);
+        g.drawString("Total Length: " + Main.totalLenght, 1000, 100);
 
 
     }
